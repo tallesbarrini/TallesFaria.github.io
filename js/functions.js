5b805381ba17e74cca633b5bc6b794bee5604885
TallesFaria <you@example.com>
2017-09-13 17:20:13 -0300

    2 Options: footer-fixed without opacity + normal footer

--- js/functions.js
+++ js/functions.js
@@ -13,26 +13,46 @@ $(window).scroll(function() {
     transform: "translate(0px, -" + (wScroll - 4000) / 2 / 40 + "%)"
   });
 
-//   if (wScroll > $(".clothes-pic").offset().top) {
-//     $(".clothes-pic img").each(function(i) {
-//       setTimeout(function() {
-//         $(".clothes-pic")
-//           .eq(i)
-//           .addClass("is-showing");
-//       }, 150 * (i + 1));
-//     });
-//   }
+  //   if (wScroll > $(".clothes-pic").offset().top) {
+  //     $(".clothes-pic img").each(function(i) {
+  //       setTimeout(function() {
+  //         $(".clothes-pic")
+  //           .eq(i)
+  //           .addClass("is-showing");
+  //       }, 150 * (i + 1));
+  //     });
+  //   }
 
-  if(wScroll > $('#page8').offset().top - $(window).height()) {
-	$('#page8').css({
-		'background-position':'center '+ (wScroll - $('#page8').offset().top) +'px' 
-	});
+  if (wScroll > $("#page8").offset().top - $(window).height()) {
+    $("#page8").css({
+      "background-position":
+        "center " + (wScroll - $("#page8").offset().top) + "px"
+    });
 
-	var opacity = (wScroll - $('#page8').offset().top + 400) / (wScroll / 15);
+    var opacity = (wScroll - $("#page8").offset().top + 400) / (wScroll / 15);
 
-	$('.window-tint').css({
-		'opacity':opacity
-	});
-	console.log(wScroll - 3000);
+    $(".window-tint").css({
+      opacity: opacity
+    });
+  }
+
+  if (wScroll > $(".blog-posts").offset().top - $(window).height()) {
+    var offset = Math.min(
+      0,
+      wScroll - $(".blog-posts").offset().top + $(window).height() - 350
+    ).toFixed();
+
+    $(".post-1").css({
+      transform: "translate(" + offset + "px, " + Math.abs(offset * 0.2) + "px)"
+    });
+
+    $(".post-3").css({
+      transform:
+        "translate(" +
+        Math.abs(offset) +
+        "px, " +
+        Math.abs(offset * 0.2) +
+        "px)"
+    });
   }
 });
